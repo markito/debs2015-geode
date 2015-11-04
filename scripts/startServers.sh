0c3927c2c15c5355e1ca73ea72bd82ce6c4a309d
William Markito <markito@users.noreply.github.com>
2015-11-04 10:23:32 -0800

    Lowering memory for server to 4GB

--- scripts/startServers.sh
+++ scripts/startServers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gfsh	-e "connect" -e "start server --name=server1 --max-heap=8G" \
+gfsh	-e "connect" -e "start server --name=server1 --max-heap=4G" \
 	-e "create region --name=TaxiTrip --type=PARTITION" \
 	-e "create region --name=FrequentRoute --type=REPLICATE" \
 	-e "create region --name=ProfitableArea --type=REPLICATE" \
