47430b832639a137a7d621d595f7ecd896f46788
Swapnil Bawaskar <sbawaskar@pivotal.io>
2016-01-20 17:45:34 -0800

    adding a CacheWriter to compute the pickup and dropoff cells.

--- listeners/src/main/java/org/apache/geode/example/debs/listeners/CellWriter.java
+++ listeners/src/main/java/org/apache/geode/example/debs/listeners/CellWriter.java
@@ -4,12 +4,18 @@ import com.gemstone.gemfire.cache.CacheWriter;
 import com.gemstone.gemfire.cache.CacheWriterException;
 import com.gemstone.gemfire.cache.EntryEvent;
 import com.gemstone.gemfire.cache.RegionEvent;
+import org.apache.geode.example.debs.model.Cell;
 import org.apache.geode.example.debs.model.TaxiTrip;
+
+import java.util.logging.Logger;
 
 /**
  * Created by sbawaskar on 1/20/16.
  */
 public class CellWriter implements CacheWriter<String, TaxiTrip> {
+
+  private static final Logger logger = Logger.getLogger(CellWriter.class.getName());
+
   private final LatLongToCellConverter latLongToCellConverter;
 
   public CellWriter(LatLongToCellConverter latLongToCellConverter) {
@@ -28,8 +34,17 @@ public class CellWriter implements CacheWriter<String, TaxiTrip> {
   @Override
   public void beforeCreate(EntryEvent<String, TaxiTrip> event) throws CacheWriterException {
     TaxiTrip trip = event.getNewValue();
-    trip.setPickup_cell(this.latLongToCellConverter.getCell(trip.getPickup_latitude(), trip.getPickup_longitude()));
-    trip.setDropoff_cell(this.latLongToCellConverter.getCell(trip.getDropoff_latitude(), trip.getDropoff_longitude()));
+    Cell pickupCell;
+    Cell dropoffCell;
+    try {
+      pickupCell = this.latLongToCellConverter.getCell(trip.getPickup_latitude(), trip.getPickup_longitude());
+      dropoffCell = this.latLongToCellConverter.getCell(trip.getDropoff_latitude(), trip.getDropoff_longitude());
+    } catch (IllegalArgumentException e) {
+      logger.info("Trip "+event.getKey()+" outside study zone");
+      throw e;
+    }
+    trip.setPickup_cell(pickupCell);
+    trip.setDropoff_cell(dropoffCell);
   }
 
   @Override
--- scripts/startServers.sh
+++ scripts/startServers.sh
@@ -13,6 +13,6 @@ gfsh	-e "connect" -e "start server --name=server1 --max-heap=4G"
 gfsh	-e "connect" \
 	-e "create async-event-queue --id=frequentRouteQueue --batch-time-interval=5 --listener=org.apache.geode.example.debs.listeners.FrequentRouterListener"\
 	-e "create async-event-queue --id=cellGridQueue --batch-time-interval=5 --listener=org.apache.geode.example.debs.listeners.CellGridListener"\
-	-e "create region --name=TaxiTrip --type=PARTITION --async-event-queue-id=frequentRouteQueue,cellGridQueue"\
+	-e "create region --name=TaxiTrip --type=PARTITION --cache-writer=org.apache.geode.example.debs.listeners.CellWriter --async-event-queue-id=frequentRouteQueue,cellGridQueue"\
 	-e "create region --name=FrequentRoute --type=REPLICATE"\
 	-e "create region --name=ProfitableArea --type=REPLICATE"\
