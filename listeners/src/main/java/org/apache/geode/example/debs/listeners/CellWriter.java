7e6e0a0ebf3b6f576045fed8cb6a8ce4d0673668
William Markito <wmarkito@pivotal.io>
2016-01-27 00:34:50 -0800

    Writing Cells through event on cacheWriter

--- listeners/src/main/java/org/apache/geode/example/debs/listeners/CellWriter.java
+++ listeners/src/main/java/org/apache/geode/example/debs/listeners/CellWriter.java
@@ -2,21 +2,30 @@ package org.apache.geode.example.debs.listeners;
 
 import com.gemstone.gemfire.cache.CacheWriter;
 import com.gemstone.gemfire.cache.CacheWriterException;
+import com.gemstone.gemfire.cache.Declarable;
 import com.gemstone.gemfire.cache.EntryEvent;
+import com.gemstone.gemfire.cache.Region;
 import com.gemstone.gemfire.cache.RegionEvent;
+import com.gemstone.gemfire.internal.cache.EntryEventImpl;
 import com.gemstone.gemfire.pdx.PdxInstance;
+import com.gemstone.gemfire.pdx.PdxInstanceFactory;
 import com.gemstone.gemfire.pdx.WritablePdxInstance;
-import org.apache.geode.example.debs.model.Cell;
-import org.apache.geode.example.debs.model.TaxiTrip;
+import com.gemstone.gemfire.pdx.internal.PdxInstanceFactoryImpl;
+import com.gemstone.gemfire.pdx.internal.json.PdxInstanceHelper;
 
-import java.util.logging.Logger;
+
+import org.apache.geode.example.debs.model.Cell;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Properties;
 
 /**
  * Created by sbawaskar on 1/20/16.
  */
-public class CellWriter implements CacheWriter<String, PdxInstance> {
+public class CellWriter implements CacheWriter<String, PdxInstance>, Declarable {
 
-  private static final Logger logger = Logger.getLogger(CellWriter.class.getName());
+  private static final Logger logger = LogManager.getLogger();
 
   private final LatLongToCellConverter latLongToCellConverter;
 
@@ -28,30 +37,50 @@ public class CellWriter implements CacheWriter<String, PdxInstance> {
     this(new LatLongToCellConverter());
   }
 
+
   @Override
-  public void beforeUpdate(EntryEvent<String, PdxInstance> event) throws CacheWriterException {
+  public void beforeCreate(EntryEvent<String, PdxInstance> event) throws CacheWriterException {
+    PdxInstance pdxInstance = event.getNewValue();
+    WritablePdxInstance writablePdxInstance = pdxInstance.createWriter();
+    Cell pickupCell;
+    Cell dropoffCell;
+
+    try {
+
+      pickupCell = this.latLongToCellConverter.getCell((double) pdxInstance.getField("pickup_latitude"), (double) pdxInstance.getField("pickup_longitude"));
+      dropoffCell = this.latLongToCellConverter.getCell((double) pdxInstance.getField("dropoff_latitude"), (double) pdxInstance.getField("dropoff_longitude"));
+
+//      PdxInstanceFactory pdxInstanceFactory = PdxInstanceFactoryImpl.newCreator("org.apache.geode.example.debs.model.Cell", false);
+//      PdxInstance pdxCellInstance = pdxInstanceFactory.create();
+
+      writablePdxInstance.setField("pickup_cell", pickupCell);
+      writablePdxInstance.setField("dropoff_cell", dropoffCell);
+
+      EntryEventImpl eei = (EntryEventImpl) event;
+      eei.setNewValue(writablePdxInstance);
+//      eei.makeSerializedNewValue();
+
+    } catch (IllegalArgumentException e) {
+      logger.info(String.format("Trip %s outside study zone", event.getKey()));
+      logger.info(String.format("Exception: %s", e.getMessage()));
+//      throw e;
+    }
+
+  }
+
+
+  @Override
+  public void close() {
 
   }
 
   @Override
-  public void beforeCreate(EntryEvent<String, PdxInstance> event) throws CacheWriterException {
-    PdxInstance trip = event.getNewValue();
-    Cell pickupCell;
-    Cell dropoffCell;
-    try {
-      double pickupLatitude = (double) trip.getField("pickup_latitude");
-      double pickupLongitude = (double) trip.getField("pickup_longitude");
-      double dropoffLatitude = (double) trip.getField("dropoff_latitude");
-      double dropoffLongitude = (double) trip.getField("dropoff_longitude");
-      pickupCell = this.latLongToCellConverter.getCell(pickupLatitude, pickupLongitude);
-      dropoffCell = this.latLongToCellConverter.getCell(dropoffLatitude, dropoffLongitude);
-    } catch (IllegalArgumentException e) {
-      logger.info("Trip "+event.getKey()+" outside study zone");
-      throw e;
-    }
-    WritablePdxInstance writablePdxInstance = trip.createWriter();
-    writablePdxInstance.setField("pickup_cell", pickupCell);
-    writablePdxInstance.setField("dropoff_cell", dropoffCell);
+  public void init(Properties props) {
+
+  }
+
+  @Override
+  public void beforeUpdate(EntryEvent<String, PdxInstance> event) throws CacheWriterException {
 
   }
 
@@ -67,11 +96,6 @@ public class CellWriter implements CacheWriter<String, PdxInstance> {
 
   @Override
   public void beforeRegionClear(RegionEvent<String, PdxInstance> event) throws CacheWriterException {
-
-  }
-
-  @Override
-  public void close() {
 
   }
 }
