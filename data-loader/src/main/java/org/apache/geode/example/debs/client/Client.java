3a48ebb14acf94adf4169ab509257c103fcb0337
Swapnil Bawaskar <sbawaskar@pivotal.io>
2016-01-30 18:37:41 -0800

    made FrequentRoute cache data on the client and registerInterest for all keys on that region. Query locally on the client for top 10 routes.

--- data-loader/src/main/java/org/apache/geode/example/debs/client/Client.java
+++ data-loader/src/main/java/org/apache/geode/example/debs/client/Client.java
@@ -3,10 +3,12 @@ package org.apache.geode.example.debs.client;
 import com.gemstone.gemfire.cache.Region;
 import com.gemstone.gemfire.cache.client.ClientCache;
 import com.gemstone.gemfire.cache.client.ClientCacheFactory;
+import com.gemstone.gemfire.cache.client.ClientRegionFactory;
 import com.gemstone.gemfire.cache.client.ClientRegionShortcut;
 import com.gemstone.gemfire.cache.query.*;
 import org.apache.geode.example.debs.config.Config;
-import org.apache.log4j.Logger;
+
+import java.util.logging.Logger;
 
 /**
  * Created by sbawaskar on 1/21/16.
@@ -25,22 +27,33 @@ public class Client {
   }
   private Region getFrequentRoutesRegion() {
     if (this.frequentRoutesRegion == null) {
-      this.frequentRoutesRegion = this.clientCache.createClientRegionFactory(ClientRegionShortcut.PROXY).create(Config.FREQUENT_ROUTES_REGION);
+      FrequentRoutesListener listener = new FrequentRoutesListener(clientCache);
+      ClientRegionFactory clientRegionFactory = this.clientCache.createClientRegionFactory(ClientRegionShortcut.CACHING_PROXY);
+      clientRegionFactory.addCacheListener(listener);
+      this.frequentRoutesRegion = clientRegionFactory.create(Config.FREQUENT_ROUTES_REGION);
+      logger.info("Region created:"+this.frequentRoutesRegion);
+      listener.registerLocalQuery();
     }
     return this.frequentRoutesRegion;
   }
 
 
-  public void registerCQ() throws CqException, CqExistsException {
+  public void registerInterest() {
+    Region frequentRoutes = getFrequentRoutesRegion();
+    frequentRoutes.registerInterestRegex(".*");
+  }
+
+  public void registerCQ() throws Exception {
 
     getFrequentRoutesRegion();
     CqAttributesFactory cqf = new CqAttributesFactory();
-    CqListener frequentRoutesListener = new FrequentRoutesListener();
+    CqListener frequentRoutesListener = new FrequentRoutesCQListener(clientCache);
     cqf.addCqListener(frequentRoutesListener);
     CqAttributes cqa = cqf.create();
     String cqName = "frequentRoutes";
     String queryStr = "SELECT * FROM /FrequentRoute";
 
+    logger.info("registering CQ ...");
     CqQuery priceTracker = this.clientCache.getQueryService().newCq(cqName, queryStr, cqa);
     try {
       SelectResults sResults = priceTracker.executeWithInitialResults();
@@ -53,17 +66,20 @@ public class Client {
         logger.info(sb.toString());
       }
     } catch (RegionNotFoundException e) {
-      logger.warn(e);
+      logger.warning(e.getLocalizedMessage());
+      throw e;
     }
+    logger.info("CQ registered");
   }
 
   public static void main(String[] args) {
     Client client = new Client();
     try {
       client.connect();
-      client.registerCQ();
+      //client.registerCQ();
+      client.registerInterest();
       Thread.sleep(1000 * 1000);
-    } catch (CqException | CqExistsException | InterruptedException e) {
+    } catch (Exception e) {
       e.printStackTrace();
     }
   }
--- data-loader/src/main/java/org/apache/geode/example/debs/client/FrequentRoutesListener.java
+++ data-loader/src/main/java/org/apache/geode/example/debs/client/FrequentRoutesListener.java
@@ -1,7 +1,57 @@
 package org.apache.geode.example.debs.client;
 
+import com.gemstone.gemfire.cache.EntryEvent;
+import com.gemstone.gemfire.cache.client.ClientCache;
+import com.gemstone.gemfire.cache.query.Query;
+import com.gemstone.gemfire.cache.query.QueryService;
+import com.gemstone.gemfire.cache.query.SelectResults;
+import com.gemstone.gemfire.cache.util.CacheListenerAdapter;
+
+import java.util.logging.Logger;
+
 /**
  * Created by sbawaskar on 1/30/16.
  */
-public class FrequentRoutesListener {
+public class FrequentRoutesListener extends CacheListenerAdapter {
+  private static final Logger logger = Logger.getLogger(FrequentRoutesListener.class.getName());
+
+  private ClientCache clientCache;
+  private Query top10Query;
+
+  public FrequentRoutesListener(ClientCache clientCache) {
+    this.clientCache = clientCache;
+  }
+
+  @Override
+  public void afterUpdate(EntryEvent event) {
+    try {
+      runLocalQuery();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public void afterCreate(EntryEvent event) {
+    try {
+      runLocalQuery();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void registerLocalQuery() {
+    QueryService queryService = clientCache.getLocalQueryService();
+    //this.top10Query = queryService.newQuery("select e.key.pickup_cell.x,e.key.pickup_cell.y,e.key.dropoff_cell,e.value.numTrips from /FrequentRoute.entries e ORDER BY e.value.numTrips DESC LIMIT 10");
+    this.top10Query = queryService.newQuery("select e.key,e.value from /FrequentRoute.entries e ORDER BY e.value.numTrips DESC LIMIT 10");
+  }
+
+  private void runLocalQuery() throws Exception {
+    logger.info("RUNNING QUERY LOCALLY");
+    SelectResults results = (SelectResults) this.top10Query.execute();
+    for (Object result : results) {
+      logger.info("QUERY RESULT: "+result);
+    }
+    logger.info("*********************************************");
+  }
 }
